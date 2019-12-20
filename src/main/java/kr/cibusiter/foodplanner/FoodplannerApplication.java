2ac5eb7ebd995e38af589b33dd5f87c848bcd210
whydda <whydda@naver.com>
2019-12-20 14:06:57 +0900

    추가

--- src/main/java/kr/cibusiter/foodplanner/FoodplannerApplication.java
+++ src/main/java/kr/cibusiter/foodplanner/FoodplannerApplication.java
@@ -2,12 +2,16 @@ package kr.cibusiter.foodplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class FoodplannerApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication api = new SpringApplication(FoodplannerApplication.class);
+        api.addListeners(new ApplicationPidFileWriter());
+        api.run(args);
 
-    public static void main(String[] args) {
-        SpringApplication.run(FoodplannerApplication.class, args);
     }
 
 }
